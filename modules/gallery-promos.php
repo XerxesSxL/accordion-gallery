479feb27ee03b62efa8abb85ec07c22dbf47fc71
Sean X. Luo <sean.x.luo@gmail.com>
2014-12-09 00:00:14 -0500

    Updates
    Updating code base.

---
+++ modules/gallery-promos.php
@@ -0,0 +1,14 @@
+<div class="gallery-promos" id="gallery-promos">
+	<div class="gallery-promos-inner" id="gallery-promos-inner">
+		<?php for( $i = 1; $i <= 4; $i++ ) { ?>
+			<div class="gallery-promo left">
+				<h3 class="promo-title italic">promo title <?php echo $i; ?></h3>
+				<p class="promo-description">
+					<a href="#" class="no-hover">lorem ipsum dolor sit amet, consectetur adipiscing elit. mauris sit amet sagittis quam. fusce risus ante, imperdiet ut auctor ac, commodo eu velit.<span class="blue-link-arrow"></span></a>
+				</p>
+				<div class="clear"></div>
+			</div>
+		<?php } ?>
+		<div class="clear"></div>
+	</div>
+</div>
